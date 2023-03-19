9407c1880e095c66a5af523c8b3191122ef768eb
vlad <vlad.gromov666@gmail.com>
2023-03-20 01:17:18 +0300

    create models and views for analysed items

--- monitoring_social/monitoring/mixins.py
+++ monitoring_social/monitoring/mixins.py
@@ -1,11 +1,33 @@
+def generate_base_menu():
+    return [
+        {
+            'name': 'Главная',
+            'code': 'main',
+            'url': ''
+        },
+        {
+            'name': 'Мониторинг',
+            'code': 'monitoring',
+            'url': ''
+        },
+        {
+            'name': 'Настройки',
+            'code': 'settings',
+            'url': ''
+        },
+    ]
+
+
 class BaseMixin:
-    menu = None
+    menu = generate_base_menu()
     title = 'Страница'
     active_menu = 0
+    show_menu = True
 
     def get_base_context(self, *args, object_list=None, **kwargs):
         context = kwargs
         context['menu'] = self.menu
         context['title'] = self.title
         context['active_menu'] = self.active_menu
+        context['show_menu'] = self.show_menu
         return context
